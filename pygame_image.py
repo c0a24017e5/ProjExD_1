58ed48affa501e67cc1c8e419bbb00d93ed5fd9f
naoki <c0a24017e5@edu.teu.ac.jp>
2025-11-18 16:42:03 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -23,7 +23,7 @@ def main():
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(1)
+        clock.tick(200)
 
 
 if __name__ == "__main__":

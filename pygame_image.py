47021c21603530f10d4f94d97b9930e645b40eec
naoki <c0a24017e5@edu.teu.ac.jp>
2025-11-18 18:19:56 +0900

    演習2 move_ip1回のみ

--- pygame_image.py
+++ pygame_image.py
@@ -27,17 +27,24 @@ def main():
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, kk_rct) #t5
         
-        kk_rct.move_ip(-1,0)
+        x_p = -1
+        y_p = 0
+        
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]: 
-            kk_rct.move_ip((0, -1))
+            x_p = 0
+            y_p = -1
         if key_lst[pg.K_DOWN]: 
-            kk_rct.move_ip((0, 1))
+            x_p = 0
+            y_p = 1
         if key_lst[pg.K_LEFT]: 
-            kk_rct.move_ip((-1, 0))
+            x_p = -1
+            y_p = 0
         if key_lst[pg.K_RIGHT]: 
-            kk_rct.move_ip((2, 0))
-               
+            x_p = 1
+            y_p = 0
+        kk_rct.move_ip(x_p,y_p)
+        
         pg.display.update()
         tmr += 1        
         clock.tick(200)

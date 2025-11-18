c55dcc5ebae88c905c814140ebb5456ee921bd31
naoki <c0a24017e5@edu.teu.ac.jp>
2025-11-18 17:43:33 +0900

    t4 moveip

--- pygame_image.py
+++ pygame_image.py
@@ -29,13 +29,13 @@ def main():
         
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]: 
-            # kk_rct.move_ip((0, -1))
+            kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]: 
-            # kk_rct.move_ip((0, 1))
+            kk_rct.move_ip((0, 1))
         if key_lst[pg.K_LEFT]: 
-            # kk_rct.move_ip((-1, 0))
+            kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]: 
-            # kk_rct.move_ip((1, 0))
+            kk_rct.move_ip((1, 0))
                
         pg.display.update()
         tmr += 1        
